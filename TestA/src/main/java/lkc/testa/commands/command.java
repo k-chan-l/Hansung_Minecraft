b1e0580a068e27f0aa07fb108104e68d42143257
k-chan-l <leekyungchan0625@gmail.com>
2022-01-28 14:28:14 +0900

    TestA,TestB
    test

--- TestA/src/main/java/lkc/testa/commands/command.java
+++ TestA/src/main/java/lkc/testa/commands/command.java
@@ -8,15 +8,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginLoader;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getServer;
 
 public class command implements CommandExecutor {
-    public TestB testB;
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -24,11 +22,7 @@ public class command implements CommandExecutor {
             return false;
         if (!(sender instanceof Player))
             return false;
-        getServer().getLogger().info("[Test Plugin A]테스트");
-        testB = (TestB) Bukkit.getPluginManager().getPlugin("TestB");
-
-
-
+        getLogger().info("[TestA --> TestB]"+TestB.num());
 
         return true;
     }
--- TestB/src/main/java/lkc/testb/TestB.java
+++ TestB/src/main/java/lkc/testb/TestB.java
@@ -4,12 +4,10 @@ import lkc.testb.something.Something;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestB extends JavaPlugin {
-    public Something something;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        something = new Something();
         getServer().getLogger().info("[Test Plugin B] enable");
     }
 
@@ -17,5 +15,9 @@ public final class TestB extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getServer().getLogger().info("[Test Plugin B] disable");
+    }
+
+    public static int num(){
+        return 300;
     }
 }
